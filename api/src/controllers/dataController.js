a1f823a675043b2f41f2f4986ae25cf20c4cd030
marcosdon28 <marcosdon28@gmail.com>
2022-08-03 11:39:43 -0300

    update controller

--- api/src/controllers/dataController.js
+++ api/src/controllers/dataController.js
@@ -2,7 +2,7 @@ const dataService = require("../services/dataService")
 
 const createData = (req, res) =>{
     const { body } = req;
-
+    if (!body.medio-de-transporte || distancia-semanal ||)
     const createdData = dataService.createData();
     res.send("devolver url del pdf");
 };
