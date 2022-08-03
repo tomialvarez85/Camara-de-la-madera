95d24c958a436076e23fdd0df09271fc5142a298
marcosdon28 <marcosdon28@gmail.com>
2022-08-03 14:36:52 -0300

    update data

--- api/src/controllers/dataController.js
+++ api/src/controllers/dataController.js
@@ -1,10 +1,27 @@
 const dataService = require("../services/dataService")
 
 const createData = (req, res) =>{
-    const { body } = req;
-
-    const createdData = dataService.createData();
-    res.send("devolver url del pdf");
+    const {body} = req;
+    
+    if (!body.transporte ||
+        !body.distancia || 
+        !body.electrodomesticos || 
+        !body.alimentos || 
+        !body.plantados)
+        {
+            res.send("send all the fields !");
+        }
+    const newData = {
+        transporte: body.transporte,
+        distancia : body.distancia,
+        electrodomesticos : body.electrodomesticos,
+        alimentos : body.aliementos,
+        plantados : body.plantados
+    };
+    
+    //const createdData = dataService.createData(newData);
+    console.log(body)
+    res.status(201).send({ status : "OK", data : body});
 };
 
 module.exports = {
