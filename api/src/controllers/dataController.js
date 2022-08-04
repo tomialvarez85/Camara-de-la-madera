2e29a3db86787c362031e36b4e1a3957bc9ed99a
marcosdon28 <marcosdon28@gmail.com>
2022-08-04 16:33:51 -0300

    update

--- api/src/controllers/dataController.js
+++ api/src/controllers/dataController.js
@@ -1,11 +1,56 @@
 const dataService = require("../services/dataService")
+var pdf = require('html-pdf');
 
 const createData = (req, res) =>{
     const { body } = req;
-    if (!body.medio-de-transporte || distancia-semanal ||)
-    const createdData = dataService.createData();
-    res.send("devolver url del pdf");
+    if (!body.vehicle ||
+        !body.distance || 
+        !body.domesticAppliances ||
+        !body.nutrition || 
+        !body.plantedTrees)
+        {
+            res.send("ningun campo puede quedar vacio!");
+        }
+    for(var i = 0; i <= 6; i++){
+        if (!body.domesticAppliances[i].name || !body.domesticAppliances[i].amount){
+            res.send("falta datos en el electrodomestico numero: " + i + " !")
+        }
+    }
+
+    newData = {
+        vehicle : body.vehicle,
+        distance : body.distance,
+        domesticAppliances : body.domesticAppliances,
+        nutrition : body.nutrition,
+        plantedTrees : body.plantedTrees
+    };
+    console.log(newData)
+    //hacer calculos
+
+
+
+
+
+
+    
+    //Generar pdf
+    var contenido = `<p>aca va la dataaaaaaa</p> ${newData.vehicle}`;
+    pdf.create(contenido).toFile('./salida.pdf', function(err, res) {
+        if (err){
+            console.log(err);
+        } else {
+            console.log(res);
+        }
+    });
+
+    res.status(201).send({ status : "OK", info : "devolver url del objeto en el s3", data : newData});
 };
+
+
+
+
+
+//subir a amazon y devolver url del objeto
 
 module.exports = {
     createData,
