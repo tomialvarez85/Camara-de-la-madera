e34908890ce7805a1b0e0f53ee2824f05f85e19c
marcosdon28 <marcosdon28@gmail.com>
2022-08-12 09:54:50 -0300

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const v1DataRouter = require ('./v1/routes/dataRoutes.js');
-
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1/", v1DataRouter);
+
+app.use(express.static('C:/Users/Usuario/Desktop/Camara-de-la-madera/front'));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("C:/Users/Usuario/Desktop/Camara-de-la-madera/front/index.html"));
+    console.log(res)
+});
+
 
 
 app.listen(PORT, () => {
