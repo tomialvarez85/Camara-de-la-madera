41a6ba0f9671f73468a3f592197e19109d412b5b
marcosdon28 <marcosdon28@gmail.com>
2022-08-14 14:34:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const v1DataRouter = require ('./src/v1/routes/dataRoutes.js');
 const path = require('path')
 
 const app = express();
-const PORT = 3000;
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1/", v1DataRouter);
+
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT} `)
