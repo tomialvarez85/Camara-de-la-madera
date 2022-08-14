ceea61081fa741da69216e539b1fe5358cba0ab4
marcosdon28 <marcosdon28@gmail.com>
2022-08-13 21:53:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const v1DataRouter = require ('./src/v1/routes/dataRoutes.js');
 const path = require('path')
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 8081;
+//const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/v1/", v1DataRouter);
 
