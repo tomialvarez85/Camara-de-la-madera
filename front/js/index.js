a8cbbadbd4ce77db80e2900565bfaf17edd9b5a4
tomialvarez85 <tomialvarez78@gmail.com>
2022-08-15 21:42:00 -0300

    front updated

--- front/js/index.js
+++ front/js/index.js
@@ -1,76 +1,131 @@
+let btnShow = document.querySelector('button');
 
-const selected_vehicle = document
-
-form = {
-    "vehicle" : "",
-    "distance" : "25",
-    "domesticAppliances" :
-        [   
-            {
-                "name" : "fridge",
-                "amount" : 2
-            },
-            {
-                "name" : "washingMachine",
-                "amount" : 1
-            },
-            {
-                "name" : "dishWasher",
-                "amount" : 1
-            },
-            {
-                "name" : "clothesDryer",
-                "amount" : 1
-            },
-            {
-                "name" : "tv",
-                "amount" : 3
-            },
-            {
-                "name" : "pc",
-                "amount" : 2
-            },
-            {
-                "name" : "stereo",
-                "amount" : 1
-            }
-        ],
-
-    "nutrition" : "carnio",
-    "plantedTrees" : "75"
-
-}
+btnShow.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (document.querySelector('input[name="vehicle"]:checked'))
+        var selectedvic = document.querySelector('input[name="vehicle"]:checked').value;
+    else
+        var selectedvic = 0
 
 
+    if (document.querySelector('input[name="distance"]:checked'))
+        var selecteddis = document.querySelector('input[name="distance"]:checked').value;
+    else
+        var selecteddis = 0
+
+//DA
+
+    if (document.querySelector('input[id="aaa"]:checked'))
+        var famount = 1
+    else 
+        var famount = 0
+
+    if (document.querySelector('input[id="bbb"]:checked'))
+        var wmamount = 1
+    else 
+        var wmamount = 0
+
+    if (document.querySelector('input[id="ccc"]:checked'))
+        var dwamount = 1
+    else 
+        var dwamount = 0
+
+    if (document.querySelector('input[id="ddd"]:checked'))
+        var cdamount = 1
+    else 
+        var cdamount = 0
 
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
+    if (document.querySelector('input[id="eee"]:checked'))
+        var tvamount = 2
+    else 
+        var tvamount = 0
 
-    const formData = new FormData(form);
+    if (document.querySelector('input[id="fff"]:checked'))
+        var pcamount = 2
+    else 
+        var pcamount = 0
 
-    const data = Object.fromEntries(formData);
-    console.log(data);
 
-    fetch('https://reqres.in/api/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            // your expected POST request payload goes here
-            title: "My post title",
-            body: "My post content."
-        })
-    })
-    .then(res => res.json())
-    .then(data => {
-    // enter you logic when the fetch is successful
-        console.log(data)
-    })
-    . catch(error => {
-    // enter your logic for when there is an error (ex. error toast)
-    console.log(error)
-    })  
+    if (document.querySelector('input[id="ggg"]:checked'))
+        var stamount = 1
+    else 
+        var stamount = 0
+    
+
+
+    let fridge = document.querySelector('input[id="aaa"]').value;
+    let washingMachine = document.querySelector('input[id="bbb"]').value;
+    let dishWasher = document.querySelector('input[id="ccc"]').value;
+    let clothesDryer = document.querySelector('input[id="ddd"]').value;
+    let tv = document.querySelector('input[id="eee"]').value;
+    let pc = document.querySelector('input[id="fff"]').value;
+    let stereo = document.querySelector('input[id="ggg"]').value;
+    
+//END DA
+
+    if (document.querySelector('input[name="nutrition"]:checked'))
+        var selectednut = document.querySelector('input[name="nutrition"]:checked').value;
+    else
+        var selectednut = 0
+
+
+    if (document.querySelector('input[name="plantedTrees"]:checked'))
+        var selectedtre = document.querySelector('input[name="plantedTrees"]:checked').value;
+    else
+        var selectedtre = 0
+
+
+    dict = {
+        "vehicle" : selectedvic,
+        "distance" : selecteddis,
+        "domesticAppliances" :
+            [   
+                {
+                    "name" : fridge,
+                    "amount" : famount
+                },
+                {
+                    "name" : washingMachine,
+                    "amount" : wmamount
+                },
+                {
+                    "name" : dishWasher,
+                    "amount" : dwamount
+                },
+                {
+                    "name" : clothesDryer,
+                    "amount" : cdamount
+                },
+                {
+                    "name" : tv,
+                    "amount" : tvamount
+                },
+                {
+                    "name" : pc,
+                    "amount" : pcamount
+                },
+                {
+                    "name" : stereo,
+                    "amount" : stamount
+                }
+            ],
+    
+        "nutrition" : selectednut,
+        "plantedTrees" : selectedtre
+    }
+
+    var url = 'https://reqres.in/api/users';
+
+    fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(dict),
+    headers:{
+        'Content-Type': 'application/json'
+    }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+
 
 });
