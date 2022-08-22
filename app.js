3f4e03ec467197dc517faa95e618668b569ddc40
marcosdon28 <marcosdon28@gmail.com>
2022-08-22 11:03:29 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const v1DataRouter = require ('./src/v1/routes/dataRoutes.js');
-const path = require('path')
+const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/", v1DataRouter);
 
 
