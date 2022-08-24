78d56504595c47f1be45ad60f9d0ced6c705fad6
marcosdon28 <marcosdon28@gmail.com>
2022-08-24 08:13:56 -0300

    update app

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ const express = require('express');
 const v1DataRouter = require ('./src/v1/routes/dataRoutes.js');
 const path = require('path');
 const cors = require('cors');
+const https = require('https')
+const fs = require('fs')
 
+const key = fs.readFileSync('private.key')
+const cert = fs.readFileSync('certificate.crt')
+
+const cred = {
+    key,
+    cert
+
+}
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1/", v1DataRouter);
+
+app.get('/' ,(req, res) =>{ 
+    res.send('<h1>hola</h1>')
+});
 
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT} `)
 
 });
+
+
+const httpsServer = https.createServer(cred, app);
+httpsServer.listen(8443)
