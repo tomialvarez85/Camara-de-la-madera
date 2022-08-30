0ebe7cfb8ff7d8ae084324eb4a1d8b247ebbd76a
tomialvarez85 <tomialvarez78@gmail.com>
2022-08-30 10:54:09 -0300

    Final Version ¿?

--- front/js/index.js
+++ front/js/index.js
@@ -1,10 +1,21 @@
 let btnShow = document.querySelector('button');
 
+
 btnShow.addEventListener('click', function (e) {
     e.preventDefault();
+      
+    if(document.querySelector('input[name="vehicle"]:checked')){
+        var selectedvic = document.querySelector('input[name="vehicle"]:checked').value;
+    }else{
+        var selectedvic = null;
+    }
 
-    let selectedvic = document.querySelector('input[name="vehicle"]:checked').value;
-    let selecteddis = document.querySelector('input[name="distance"]:checked').value;
+    if(document.querySelector('input[name="distance"]:checked')){
+        var selecteddis = document.querySelector('input[name="distance"]:checked').value;
+    }else{
+        var selecteddis = null;
+    }
+
 
     //DA
 
@@ -56,103 +67,111 @@ btnShow.addEventListener('click', function (e) {
     
     //END DA
 
-    let selectednut = document.querySelector('input[name="nutrition"]:checked').value;
-    let selectedtre = document.querySelector('input[name="plantedTrees"]:checked').value;
-    
-
-    const dict = {
-        "vehicle" : selectedvic,
-        "distance" : selecteddis,
-        "domesticAppliances" :
-            [   
-                {
-                    "name" : fridge,
-                    "amount" : famount
-                },
-                {
-                    "name" : washingMachine,
-                    "amount" : wmamount
-                },
-                {
-                    "name" : dishWasher,
-                    "amount" : dwamount
-                },
-                {
-                    "name" : clothesDryer,
-                    "amount" : cdamount
-                },
-                {
-                    "name" : tv,
-                    "amount" : tvamount
-                },
-                {
-                    "name" : pc,
-                    "amount" : pcamount
-                },
-                {
-                    "name" : stereo,
-                    "amount" : stamount
-                }
-            ],
-    
-        "nutrition" : selectednut,
-        "plantedTrees" : selectedtre
+    if(document.querySelector('input[name="nutrition"]:checked')){
+        var selectednut = document.querySelector('input[name="nutrition"]:checked').value;
+    }else{
+        var selectednut = null;
     }
 
-    console.log(dict);
+    if(document.querySelector('input[name="plantedTrees"]:checked')){
+        var selectedtre = document.querySelector('input[name="plantedTrees"]:checked').value;
+    }else{
+        var selectedtre = null;
+    }
 
-    document.getElementById('bodyform').style = 'backdrop-filter: blur(10px); background-color: rgba(255, 255, 255, 0.3);';
-    document.getElementById('ukcontainer').style.display = 'flex';
-    document.getElementById('output').style.display = 'block';
-    document.getElementById('btnenviar').style.display = 'none';
-    document.getElementById('decobtn').style.display = 'block';
-    document.getElementById('decoh').style.display = 'block';
-    document.getElementById('replacedp').style.display = 'block';
-    const main = document.getElementById('mainid');
-    main.remove();
-
-
-    var timeleft = 35;
-    var downloadTimer = setInterval(function(){
-        if(timeleft <= 0){
-            clearInterval(downloadTimer);
-            window.location.replace("file:///home/tomas/huellacarbono/index.html");
-        } else {
-        document.getElementById("countdown").innerHTML = timeleft;
+    if(selectedtre === null || selectednut === null || selecteddis === null || selectedvic === null){
+        alert("Porfavor complete todos los campos");
+    }else{
+        const dict = {
+            "vehicle" : selectedvic,
+            "distance" : selecteddis,
+            "domesticAppliances" :
+                [   
+                    {
+                        "name" : fridge,
+                        "amount" : famount
+                    },
+                    {
+                        "name" : washingMachine,
+                        "amount" : wmamount
+                    },
+                    {
+                        "name" : dishWasher,
+                        "amount" : dwamount
+                    },
+                    {
+                        "name" : clothesDryer,
+                        "amount" : cdamount
+                    },
+                    {
+                        "name" : tv,
+                        "amount" : tvamount
+                    },
+                    {
+                        "name" : pc,
+                        "amount" : pcamount
+                    },
+                    {
+                        "name" : stereo,
+                        "amount" : stamount
+                    }
+                ],
+        
+            "nutrition" : selectednut,
+            "plantedTrees" : selectedtre
         }
-        timeleft -= 1;
-    }, 1000);
+
+        console.log(dict);
 
 
-    let obj;
-    let serverurl;
+        document.getElementById('bodyform').style = 'backdrop-filter: blur(10px); background-color: rgba(255, 255, 255, 0.3);';
+        document.getElementById('ukcontainer').style.display = 'flex';
+        document.getElementById('output').style.display = 'block';
+        document.getElementById('btnenviar').style.display = 'none';
+        document.getElementById('decobtn').style.display = 'block';
+        document.getElementById('decoh').style.display = 'block';
+        document.getElementById('replacedp').style.display = 'block';
+        const main = document.getElementById('mainid');
+        main.remove();
 
-    fetch('https://54.94.99.97/api/v1/data/', {  
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(dict)
-    }) 
-    .then(res => res.json())
-    .then(data => obj = data)
-    .then(function(data) {
-        //console.log(obj);
-        serverurl = data.objectURL;
-        console.log(serverurl);
-        let qrcode = new QRCode("output", {
-            text: serverurl,
-            width: 177,
-            height: 177,
-            colorDark : "#000000",
-            colorLight : "#ffffff",
-            correctLevel : QRCode.CorrectLevel.H
+
+        var timeleft = 30;
+        var downloadTimer = setInterval(function(){
+            if(timeleft <= 0){
+                clearInterval(downloadTimer);
+                window.location.replace("file:///home/tomas/huellacarbono/index.html");
+            } else {
+            document.getElementById("countdown").innerHTML = timeleft;
+            }
+            timeleft -= 1;
+        }, 1000);
+
+
+        let obj;
+        let serverurl;
+
+        fetch('https://54.94.99.97/api/v1/data/', {  
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(dict)
+        }) 
+        .then(res => res.json())
+        .then(data => obj = data)
+        .then(function(data) {
+            //console.log(obj);
+            serverurl = data.objectURL;
+            console.log(serverurl);
+            let qrcode = new QRCode("output", {
+                text: serverurl,
+                width: 177,
+                height: 177,
+                colorDark : "#000000",
+                colorLight : "#ffffff",
+                correctLevel : QRCode.CorrectLevel.H
+            });
+            document.getElementById('replacedp').style.display = 'none';
+            document.getElementById('decooutput').style.display = 'flex';
+            document.getElementById('countdown').style.display = 'block';
         });
-        document.getElementById('replacedp').style.display = 'none';
-        document.getElementById('decooutput').style.display = 'flex';
-        document.getElementById('countdown').style.display = 'block';
-    });
-
-    /*setTimeout(function(){
-        window.location.replace("file:///home/tomas/huellacarbono/index.html");
-    }, 30000);*/
-
+    }
 });
